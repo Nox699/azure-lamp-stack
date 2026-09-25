4eff0510c2b6fce0b1575fe66c94b419219ed15f
Nox699 <nvejs@live.dk>
2026-09-25 09:59:08 +0200

    Update terraform/variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,39 @@
+variable "project_name" {
+  description = "Prefix used for Azure resource names."
+  type        = string
+  default     = "lamp-school"
+}
+
+variable "location" {
+  description = "Azure region."
+  type        = string
+  default     = "Sweden Central"
+}
+
 variable "vm_size" {
-  type    = string
-  default = "Standard_B2als_v2"
+  description = "Azure VM size. B2als_v2 is the selected Sweden Central size for Docker + Apache/PHP + MariaDB."
+  type        = string
+  default     = "Standard_B2als_v2"
 }
 
 variable "admin_username" {
-  type    = string
-  default = "azureadmin"
+  description = "Linux administrator user."
+  type        = string
+  default     = "azureadmin"
 }
 
 variable "ssh_public_key_path" {
-  type    = string
-  default = "~/.ssh/id_ed25519.pub"
+  description = "Path to the local SSH public key used for VM login."
+  type        = string
+  default     = "~/.ssh/azure-lamp.pub"
 }
 
 variable "admin_cidr" {
-  type = string
+  description = "Public administrator IP/CIDR allowed to SSH, for example 203.0.113.10/32. Do not use 0.0.0.0/0 unless temporarily required."
+  type        = string
+
   validation {
     condition     = can(cidrnetmask(var.admin_cidr))
-    error_message = "admin_cidr must be a valid CIDR, e.g. 203.0.113.10/32."
+    error_message = "admin_cidr must be a valid IPv4/IPv6 CIDR such as 203.0.113.10/32."
   }
 }
