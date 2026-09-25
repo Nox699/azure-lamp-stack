d0747705d0f0c0dbe82da8153ffa20353cc4c95c
Nox699 <nvejs@live.dk>
2026-09-25 09:59:37 +0200

    Update terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,14 @@
 output "public_ip" {
-  value = azurerm_public_ip.main.ip_address
+  description = "Public IPv4 address of the LAMP VM."
+  value       = azurerm_public_ip.main.ip_address
 }
 
 output "ssh_command" {
-  value = "ssh ${var.admin_username}@${azurerm_public_ip.main.ip_address}"
+  description = "Example SSH command."
+  value       = "ssh -i ${trimsuffix(var.ssh_public_key_path, ".pub")} ${var.admin_username}@${azurerm_public_ip.main.ip_address}"
+}
+
+output "web_url" {
+  description = "HTTP URL for the deployed application."
+  value       = "http://${azurerm_public_ip.main.ip_address}"
 }
